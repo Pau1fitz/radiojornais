a51705480a1f841ecf969e58f3e3e221743e8913
Paul Fitzgerald <paulfitz99@gmail.com>
2023-04-14 13:52:52 +0100

    Initial Commit

---
+++ radiojornais/radiojornaisApp.swift
@@ -0,0 +1,17 @@
+//
+//  radiojornaisApp.swift
+//  radiojornais
+//
+//  Created by Paul Fitzgerald on 14/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct radiojornaisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
